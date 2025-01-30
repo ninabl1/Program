ed094c50900e69262dc393e40e953f152f5cf914
ninabl1 <152603093+ninabl1@users.noreply.github.com>
2025-01-30 08:40:56 +0100

    Update program.c

--- program.c
+++ program.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
-#define MAX 100
 
-void unesiMatricu(int m[][MAX], int n) {
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            scanf("%d", &m[i][j]);
-}
-
-void ispisiMatricu(int m[][MAX], int n) {
-    for (int i = 0; i < n; i++, printf("\n"))
-        for (int j = 0; j < n; j++)
-            printf("%d ", m[i][j]);
-}
-
-int main() {
-    int n, m[MAX][MAX];
-    scanf("%d", &n);
-    unesiMatricu(m, n);
-    ispisiMatricu(m, n);
+int main(void)
+{
+    int m;
+    printf("Unesite dimenziju kvadratne matrice(m*m): ");
+    scanf("%d", &m);
+    
+    int iMatrica[m][m];
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            printf("iMatrica [%d,%d] ---> ", i, j);
+            scanf("%d", &iMatrica[i][j]);
+        }
+    }
+    
     printf("\n");
-    for (int i = 0; i < n; i++, printf("\n"))
-        for (int j = 0; j < n; j++)
-            printf("%d ", m[j][i]);
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = m - 1; j >= 0; j--)
+        {
+            printf("%d ", iMatrica[i][j]);
+        }
+        printf("\n");
+    }
+    
     return 0;
 }
